a3408c2d9d3e645a690656c2841fee7120f1c2ff
daniel <dev.danielcw@gmail.com>
2023-07-20 14:48:27 -0500

    project box edits

--- components/work.js
+++ components/work.js
@@ -1,42 +1,29 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Chip } from '@mui/material';
+import { Container } from '@mui/material';
+import Laptop from "../images/laptop.webp"
+const Work = (props) => {
+  return (
+    <>
+        <Card sx={{ width: 345, marginX: "auto" }}>
+            <CardMedia sx={{ height: 140 }} image={`${Laptop.src}`} title="green iguana" />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div"> {props.project} </Typography>
+                <Typography variant="body2" color="text.secondary"> {props.description} </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small"> Source </Button>
+                <Button size="small"> Demo </Button>
+            </CardActions>
+        </Card>        
+    </>
 
-const Work = () => {
-    return (
-            <Card sx={{ minWidth: 275 }}>
-
-                <CardContent>
-                    <Typography variant="h4"> Weather App </Typography>
-                </CardContent>
-
-                <CardContent>
-                    <Typography variant="body2"> 
-                        This project was made to render weather forecast data based on the location provided by user input.
-                        It utilizes Javascript, React, Tailwind CSS and REST API
-                    </Typography>
-                    <Button> Source </Button>
-                    <Button> Demo </Button>
-
-                    <div>
-                        <Chip label="JS"></Chip>
-                        <Chip label="React"></Chip> 
-                        <Chip label="Tailwind"></Chip>                        
-                        <Chip label="REST API"></Chip>                        
-                        <Chip label="HTML/CSS"></Chip>                        
-                    </div>
-
-
-                </CardContent>
-
-            </Card>        
-
-    )
+  );
 }
 
 export default Work
--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 import Header from "@/components/header"
 import laptop2 from "../images/laptop2.jpg"
-import CardContainer from "@/components/cardContainer"
-
+import Work from "@/components/work"
+import { Container } from "@mui/material"
 
 const Projects = () => {
     return (
@@ -13,7 +13,14 @@ const Projects = () => {
             cta=""
             img={laptop2}
           />
-          <CardContainer />
+          <Container sx={{ p:5 }} className="flex flex-col md:flex-row flex-wrap gap-10">
+            <Work project="Weather App" description="Check out some of my favorite projects! Each one played a part in making me a better developer. adasdasdasdasdasdsa"/>
+            <Work project="Tic Tac Toe" description="a part in making me a better developer. adasdasdasdasdasdsa"/>
+            <Work project="Weather App" description="Check out some of my favorite projects! Each one played a part in making me a better developer. adasdasdasdasdasdsa"/>
+            <Work project="Weather App" description="Check out some of my favorite projects! Each one played a part in making me a better developer. adasdasdasdasdasdsa"/>
+            <Work project="React Sorting App" description="Check out some of my favorite projects! Each one played a  a better developer. adasdasdasdasdasdsa"/>
+          </Container>
+
         </>
     )
 }
