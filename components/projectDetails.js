61b7598d3b97d5a9f1b65b8e9c72216ad25a5847
daniel <dev.danielcw@gmail.com>
2023-07-21 13:54:07 -0500

    edits

--- components/projectDetails.js
+++ components/projectDetails.js
@@ -1,6 +1,14 @@
 import laptop from "../images/laptop.webp"
 import laptop2 from "../images/laptop2.jpg"
-
+import Sort from "../images/Sort2.PNG"
+import Weather from "../images/Weather2.PNG"
+import Finder from "../images/github_finder.png"
+import Todo from "../images/Todo2.PNG"
+import Gallery from "../images/Gallery.PNG"
+import Calc from "../images/Calc2.PNG"
+import Game from "../images/Game2.PNG"
+import Tic from "../images/Tic2.PNG"
+import Directory from "../images/Directory.PNG"
 
 const list = [
     {
@@ -8,7 +16,7 @@ const list = [
         description: "A React-based app that provides a weather forecast location data based on user input.",
         sourceCode: "https://github.com/DanielCW7/weather-app-v1/",
         demo: "https://danielcw7.github.io/weather-app-v1/",
-        thumbnail: null,
+        thumbnail: Weather.src,
         badges: [ "React", "JS", "HTML/CSS", "Tailwind CSS"]
     },
     {
@@ -16,7 +24,7 @@ const list = [
         description: "This application takes pre-defined sets of data and sorts them from highest to lowest using the \"bubble sort\" method.",
         sourceCode: "https://github.com/DanielCW7/sorter/",
         demo: "https://danielcw7.github.io/sorter/",
-        thumbnail: laptop.src,
+        thumbnail: Sort.src,
         badges: [ "React", "JS", "HTML/CSS"]
     },
     {
@@ -24,7 +32,7 @@ const list = [
         description: "A React-based app that is designed to search for other users and their profile information on Github.",
         sourceCode: "https://github.com/DanielCW7/github_finder/",
         demo: "https://danielcw7.github.io/github_finder/",
-        thumbnail: laptop2.src,
+        thumbnail: Finder.src,
         badges: [ "React", "JS", "HTML/CSS", "REST API"]
     },
     {
@@ -32,7 +40,7 @@ const list = [
         description: "A simple todo app built with React that saves todos in the current browser's local storage.",
         sourceCode: "https://github.com/DanielCW7/todo/",
         demo: "https://danielcw7.github.io/todo/",
-        thumbnail: laptop2.src,
+        thumbnail: Todo.src,
         badges: [ "React", "JS", "HTML/CSS"]
     },
     {
@@ -40,7 +48,7 @@ const list = [
         description: "This app allows a user to search through an array of photos using array iteration methods",
         sourceCode: "https://github.com/DanielCW7/gallery/",
         demo: "https://danielcw7.github.io/gallery/",
-        thumbnail: laptop2.src,
+        thumbnail: Gallery.src,
         badges: [ "JS", "HTML/CSS"]
     },
     {
@@ -48,7 +56,7 @@ const list = [
         description: "This app calculates how much concrete someone would need for their project based on provided dimensions.",
         sourceCode: "https://github.com/DanielCW7/yield_calculator/",
         demo: "https://danielcw7.github.io/yield_calculator/",
-        thumbnail: laptop2.src,
+        thumbnail: Calc.src,
         badges: [ "JS", "HTML/CSS"]
     },
     {
@@ -56,7 +64,7 @@ const list = [
         description: "A hangman-style game of guessing popular words and phrases.",
         sourceCode: "https://github.com/DanielCW7/gameshow/",
         demo: "https://danielcw7.github.io/gameshow/",
-        thumbnail: laptop2.src,
+        thumbnail: Game.src,
         badges: [ "JS", "HTML/CSS"]
     },
     {
@@ -64,7 +72,7 @@ const list = [
         description: "A simple game of tictactoe utilizing JQuery.",
         sourceCode: "https://github.com/DanielCW7/tictactoe/",
         demo: "https://danielcw7.github.io/tictactoe/",
-        thumbnail: laptop2.src,
+        thumbnail: Tic.src,
         badges: [ "JS", "JQuery", "HTML/CSS" ]
     },
     {
@@ -72,7 +80,7 @@ const list = [
         description: "Opening this app fetches randomly generated employee data from an API.",
         sourceCode: "https://github.com/DanielCW7/directory/",
         demo: "https://danielcw7.github.io/directory/",
-        thumbnail: laptop2.src,
+        thumbnail: Directory.src,
         badges: [ "JS", "HTML/CSS"]
     }
 ]
