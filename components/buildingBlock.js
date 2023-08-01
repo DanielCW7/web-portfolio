6bdb57cd78ab56da72008de6979b7a7ad1beb408
daniel <dev.danielcw@gmail.com>
2023-08-01 17:30:11 -0500

    adding animated section

--- components/buildingBlock.js
+++ components/buildingBlock.js
@@ -1,26 +1,42 @@
-import { Box, Container, Typography, Item, Grid } from "@mui/material"
-import Image from "next/image";
-import Link from "next/link";
+import { useState, useEffect } from "react";
+import { Box, Container, Typography } from "@mui/material";
+
+const adjectives = ["versatile", "ambitious", "adaptable", "reliable"];
 
 const BuildingBlock = () => {
-    return (
-        <Box className="bg-cover bg-fixed">
-            <Container className="gap-10 flex flex-col md:flex-row justify-center" sx={{
-                minHeight: {
-                    xs: "40vh",
-                    sm: "40vh",
-                    md: "45vh",  
-                },
-                p: 5
-            }}>
-                <Box className="flex-1 flex flex-col justify-center text-center ">
-                    {/* different font type? */}
-                    {/* "typewriter" function on w3schools */}
-                    <Typography className="font-black" variant="h1" component="h2" gutterBottom> I am "motivated" </Typography>
-                </Box>                    
-            </Container>             
-        </Box>
-    )
-}
+  const [currentAdjective, setCurrentAdjective] = useState(adjectives[0]);
+  const [index, setIndex] = useState(0);
 
-export default BuildingBlock
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % adjectives.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    setCurrentAdjective(adjectives[index]);
+  }, [index]);
+
+  return (
+    <Box className="bg-cover bg-fixed">
+      <Container className="gap-10 flex flex-col md:flex-row justify-center" sx={{
+        minHeight: {
+          xs: "40vh",
+          sm: "40vh",
+          md: "45vh",  
+        },
+        p: 5
+      }}>
+        <Box className="flex-1 flex flex-col justify-center text-center ">
+          <Typography className="font-black" variant="h1" component="h2" gutterBottom>
+            I am <span className="fade-in-out">{currentAdjective}</span>
+          </Typography>
+        </Box>                    
+      </Container>             
+    </Box>
+  );
+};
+
+export default BuildingBlock;
