e5d51da7ff4298a58b1ac8d4af5aef09c8ccdfcc
daniel <dev.danielcw@gmail.com>
2023-08-03 16:18:13 -0500

    mobile edit

--- components/aboutBlurb.js
+++ components/aboutBlurb.js
@@ -16,57 +16,6 @@ const AboutBlurb = () => {
                 p: 5
             }}>
 
-{/* desktop */}
-                <Box className="hidden md:flex border-2 border-red-600">
-                    <Box className="flex flex-col gap-12 flex-1">
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                            <DataObjectIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Web Engineering </Typography>
-                        </Box>                    
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <BrushIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Design </Typography>
-                        </Box> 
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <GroupIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Teamwork </Typography>
-                        </Box> 
-                    </Box>
-
-                    <Box className="hidden md:flex flex-col flex-1">
-                        <Typography className="text-center m-auto" variant="h3"  sx={{
-                            fontSize: {
-                                xs: "2rem", 
-                                sm: "3rem", 
-                                md: "4rem"
-                            },
-                            fontWeight: "bold"
-                        }}> My Strengths </Typography>
-                    </Box>
-
-                    <Box className="flex flex-col gap-12 flex-1">
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                            <DataObjectIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Responsive Design </Typography>
-
-                        </Box>                    
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <BrushIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Design </Typography>
-    
-                        </Box> 
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <GroupIcon className="text-5xl mx-auto" gutterBottom />
-                            <Typography variant="h5" component="h3" gutterBottom> Teamwork </Typography>
-                        </Box>                     
-                    </Box>                    
-                </Box>
-
-
 {/* mobile */}
                 <Box className="flex md:hidden">
                 <Typography className="text-center m-auto" variant="h3"  sx={{
@@ -95,6 +44,24 @@ const AboutBlurb = () => {
                         <GroupIcon className="text-4xl mx-auto" gutterBottom />
                             <Typography variant="h7" component="p" gutterBottom> Teamwork </Typography>
                         </Box> 
+
+                        <Box className="flex-1 flex flex-col justify-center text-center"> 
+                            <DataObjectIcon className="text-4xl mx-auto" gutterBottom />
+                            <Typography variant="h7" component="p" gutterBottom> Responsive Design </Typography>
+
+                        </Box>   
+
+                        <Box className="flex-1 flex flex-col justify-center text-center"> 
+                        <BrushIcon className="text-4xl mx-auto" gutterBottom />
+                            <Typography variant="h7" component="p" gutterBottom> Design </Typography>
+    
+                        </Box>   
+
+                        <Box className="flex-1 flex flex-col justify-center text-center"> 
+                        <GroupIcon className="text-4xl mx-auto" gutterBottom />
+                            <Typography variant="h7" component="p" gutterBottom> Teamwork </Typography>
+                        </Box>                          
+
                     </Box>
 
                     <Box className="hidden md:flex flex-col flex-1">
@@ -105,25 +72,7 @@ const AboutBlurb = () => {
                             fontWeight: "bold"
                         }}> My Strengths </Typography>
                     </Box>
-
-                    <Box className="flex flex-col gap-12 flex-1">
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                            <DataObjectIcon className="text-4xl mx-auto" gutterBottom />
-                            <Typography variant="h7" component="p" gutterBottom> Responsive Design </Typography>
-
-                        </Box>                    
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <BrushIcon className="text-4xl mx-auto" gutterBottom />
-                            <Typography variant="h7" component="p" gutterBottom> Design </Typography>
-    
-                        </Box> 
-
-                        <Box className="flex-1 flex flex-col justify-center text-center"> 
-                        <GroupIcon className="text-4xl mx-auto" gutterBottom />
-                            <Typography variant="h7" component="p" gutterBottom> Teamwork </Typography>
-                        </Box>                     
-                    </Box>                    
+                   
                 </Box>
 
 
