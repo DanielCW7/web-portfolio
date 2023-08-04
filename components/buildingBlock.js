73c4727d2b792b846674461f84fb265a161d1a30
daniel <dev.danielcw@gmail.com>
2023-08-04 00:00:37 -0500

    typing function built, next delete function

--- components/buildingBlock.js
+++ components/buildingBlock.js
@@ -1,23 +1,40 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import { Box, Container, Typography } from "@mui/material";
 const adjectives = ["versatile", "ambitious", "adaptable", "reliable"];
 
 const BuildingBlock = () => {
-  const [currentAdjective, setCurrentAdjective] = useState(adjectives[0]);
-  const [index, setIndex] = useState(0);
+  // const [currentAdjective, setCurrentAdjective] = useState(adjectives[0]);
+  // const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % adjectives.length);
-    }, 4000);
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setIndex((prevIndex) => (prevIndex + 1) % adjectives.length);
+  //   }, 6000);
 
-    return () => clearInterval(intervalId);
-  }, []);
+  //   return () => clearInterval(intervalId);
+  // }, []);
 
-  useEffect(() => {
-    setCurrentAdjective(adjectives[index]);
-  }, [index]);
+  // useEffect(() => {
+  //   setCurrentAdjective(adjectives[index]);
+  // }, [index]);
 
+  
+const [word, changeWord] = useState(adjectives[0])
+
+useEffect(() => {
+  function adj() {
+    const target = document.querySelector(".typewriter");
+
+    // types out the entire word and then exits the function - need to create a delete counterpart and then change the word, restarting the function
+    for(let i = 0; i < word.length; i++) {
+      setTimeout(() => {
+        target.innerHTML += word[i]
+      }, 250 * i)
+    }
+  } 
+  
+  adj()
+})
 
 
   return (
@@ -30,9 +47,9 @@ const BuildingBlock = () => {
         },
         p: 5
       }}>
-        <Box className="flex-1 flex flex-col justify-center text-center ">
+        <Box className="flex-1 flex flex-col justify-center text-center">
           <Typography className="font-black" variant="h1" component="h2" gutterBottom>
-            I am <span className="example">{currentAdjective}</span>
+            I am <span className="typewriter"></span>
           </Typography>
         </Box>                    
       </Container>             
