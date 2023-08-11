680f27655f6170c05a3bcb2ba3244dcc8ffd45b0
daniel <dev.danielcw@gmail.com>
2023-08-11 18:54:45 -0500

    animating the headers

--- components/header.js
+++ components/header.js
@@ -11,29 +11,61 @@ import Image from "next/image";
 const Header = (props) => {
 
     useEffect(() => {
-            anime({
-                targets: "#title",
-                keyframes: [
-                    {translateX: 50, opacity: 0},
-                    {translateX: 0, opacity: 1}
-                ],
-                easing: "easeOutExpo",
-                loop: false
-            })
+        let timeLine = anime.timeline({
+            duration: 500
+        })
+
+        timeLine   
+        .add({
+            targets: "#title",
+            keyframes: [
+                {translateX: 50, opacity: 0},
+                {translateX: 0, opacity: 1}
+            ],
+            easing: "easeOutExpo",
+            loop: false
+        }, 0)
+
+        .add({
+            targets: "#subtitle",
+            keyframes: [
+                {translateX: 50, opacity: 0},
+                {translateX: 0, opacity: 1}
+            ],
+            easing: "easeOutExpo",
+        }, 50)
+
+        .add({
+            targets: "#link1",
+            keyframes: [
+                {translateX: 50, opacity: 0},
+                {translateX: 0, opacity: 1}
+            ],
+            easing: "easeOutExpo",
+        }, 150)
+
+        .add({
+            targets: "#link2",
+            keyframes: [
+                {translateX: 50, opacity: 0},
+                {translateX: 0, opacity: 1}
+            ],
+            easing: "easeOutExpo",
+        }, 250)
 
             
     }, [])
     
     return (
-        <header>
+        <header className="min-h-70 overflow-hidden">
             {/* <Image src={props.img.src} width={500} height={500} className="absolute z-10" /> */}
             <Box sx={{
-                backgroundImage: `linear-gradient(rgb(12, 12, 12, .7), rgb(12, 12, 12, .7)), url(${props.img.src})`,
+                // backgroundImage: `linear-gradient(rgb(12, 12, 12, .7), rgb(12, 12, 12, .7)), url(${props.img.src})`,
                 minHeight: "70vh",
-                backgroundPosition: 'center'
             }} className="w-full relative bg-cover bg-sticky bg-no-repeat flex flex-col justify-center">
+                
+                <Image src={props.img} layout="responsive" className="absolute object-cover header_Img" />
                 <Container sx={{ p:5 }}>
-                    
                     <Typography sx={{
                         fontSize: {
                             xs: "3rem", 
@@ -44,16 +76,16 @@ const Header = (props) => {
                         fontWeight: "bold"
                     }} variant="h1" id="title" className="opacity-0"> {props?.title ?? ""} </Typography>
 
-                    <Typography variant="overline" component="h2" className="text-white" gutterBottom id="subtitle"> 
+                    <Typography variant="overline" component="h2" className="text-white opacity-0" gutterBottom > 
                         {props.link === "/" ? "Front-end Engineer" : "" }
                     </Typography> 
-                    <Typography sx={{color: "#fff"}} gutterBottom> {props?.subtitle ?? ""} </Typography> 
+                    <Typography sx={{color: "#fff"}} id="subtitle" className="relative opacity-0" gutterBottom> {props?.subtitle ?? ""} </Typography> 
                     <Box className="flex gap-2 justify-start">
-                        <Link href="https://github.com/DanielCW7"><GitHubIcon className="text-white" /></Link>
-                        <Link href="https://www.linkedin.com/in/daniel-connor-wilson-a67706177"><LinkedInIcon className="text-white" /></Link>
+                        <Link href="https://github.com/DanielCW7"><GitHubIcon id="link1" className="text-white relative opacity-0" /></Link>
+                        <Link href="https://www.linkedin.com/in/daniel-connor-wilson-a67706177"><LinkedInIcon id="link2" className="text-white relative opacity-0" /></Link>
                     </Box>
                     { props.cta 
-                    ? <Link href={props.link}><Button variant="contain" sx={{ mt: 5}} className="glow-btn mt-5"> {props.cta} </Button></Link>
+                    ? <Link href={props.link}><Button id="cta" variant="contain" sx={{ mt: 5}} className="glow-btn mt-5"> {props.cta} </Button></Link>
                     : null }
                 </Container>
 
